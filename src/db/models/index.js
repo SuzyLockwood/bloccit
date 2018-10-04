29d00bd80b8889405b7d61565b8f7f802f52d4ad
Suzy <susan.m.mullin@gmail.com>
2018-10-04 13:39:08 -0700

    checking in changes so far so I can copy migrations in another branch

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env]; //may need to change to 'test' when testing instead of env
+var config = require(__dirname + '/../config/config.json')['test']; //may need to change to 'test' when testing instead of env
 var db = {};
 
 if (config.use_env_variable) {
