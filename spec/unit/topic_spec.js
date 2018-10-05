394a0d7004e4b79f0a5c354c1d96a53c11203928
Suzy <susan.m.mullin@gmail.com>
2018-10-05 16:41:51 -0700

    checkpoint-10-authorization: Completed

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -3,19 +3,17 @@ const Topic = require('../../src/db/models').Topic;
 const Post = require('../../src/db/models').Post;
 const User = require('../../src/db/models').User;
 
-describe('Post', () => {
+describe('Topic', () => {
   beforeEach(done => {
     this.topic;
     this.post;
     this.user;
-
     sequelize.sync({ force: true }).then(res => {
       User.create({
         email: 'starman@tesla.com',
         password: 'Trekkie4lyfe'
       }).then(user => {
-        this.user = user; //store the user
-
+        this.user = user;
         Topic.create(
           {
             title: 'Expeditions to Alpha Centauri',
@@ -36,8 +34,8 @@ describe('Post', () => {
             }
           }
         ).then(topic => {
-          this.topic = topic; //store the topic
-          this.post = topic.posts[0]; //store the post
+          this.topic = topic;
+          this.post = topic.posts[0];
           done();
         });
       });
@@ -45,20 +43,16 @@ describe('Post', () => {
   });
 
   describe('#create()', () => {
-    it('should create a post object with a title, body, and assigned topic and user', done => {
-      Post.create({
-        title: 'Pros of Cryosleep during the long journey',
-        body: "1. Not having to answer the 'are we there yet?' question.",
-        topicId: this.topic.id,
-        userId: this.user.id
+    it('should create a topic object with a title, description, and assigned posts', done => {
+      Topic.create({
+        title: 'Science Haven',
+        description: 'A place to discuss all things science'
       })
-        .then(post => {
-          expect(post.title).toBe('Pros of Cryosleep during the long journey');
-          expect(post.body).toBe(
-            "1. Not having to answer the 'are we there yet?' question."
+        .then(topic => {
+          expect(topic.title).toBe('Science Haven');
+          expect(topic.description).toBe(
+            'A place to discuss all things science'
           );
-          expect(post.topicId).toBe(this.topic.id);
-          expect(post.userId).toBe(this.user.id);
           done();
         })
         .catch(err => {
@@ -66,11 +60,11 @@ describe('Post', () => {
           done();
         });
     });
-    it('should not create a post with missing title, body, or assigned topic', done => {
-      Post.create({
-        title: 'Pros of Cryosleep during the long journey'
+    it('should not create a topic with missing title and description', done => {
+      Topic.create({
+        title: 'Science Haven'
       })
-        .then(post => {
+        .then(topic => {
           // the code in this block will not be evaluated since the validation error
           // will skip it. Instead, we'll catch the error in the catch block below
           // and set the expectations there
@@ -78,8 +72,7 @@ describe('Post', () => {
           done();
         })
         .catch(err => {
-          expect(err.message).toContain('Post.body cannot be null');
-          expect(err.message).toContain('Post.topicId cannot be null');
+          expect(err.message).toContain('Topic.description cannot be null');
           done();
         });
     });
@@ -98,34 +91,10 @@ describe('Post', () => {
       });
     });
   });
-  describe('#getTopic()', () => {
-    it('should return the associated topic', done => {
-      this.post.getTopic().then(associatedTopic => {
-        expect(associatedTopic.title).toBe('Expeditions to Alpha Centauri');
-        done();
-      });
-    });
-  });
-  describe('#setUser()', () => {
-    it('should associate a post and a user together', done => {
-      User.create({
-        email: 'ada@example.com',
-        password: 'password'
-      }).then(newUser => {
-        expect(this.post.userId).toBe(this.user.id);
-
-        this.post.setUser(newUser).then(post => {
-          expect(this.post.userId).toBe(newUser.id);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('#getUser()', () => {
-    it('should return the associated topic', done => {
-      this.post.getUser().then(associatedUser => {
-        expect(associatedUser.email).toBe('starman@tesla.com');
+  describe('#getPosts()', () => {
+    it('should return the associated posts', done => {
+      this.topic.getPosts().then(posts => {
+        expect(posts[0].title).toBe('My first visit to Proxima Centauri b');
         done();
       });
     });
